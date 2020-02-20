2d28fa7750343fb284df6852a760f0f40a4f871e
VDS13 <mit_vjazin@mail.ru>
2020-02-20 19:44:23 +0300

    fix

--- sql.sql
+++ sql.sql
@@ -222,11 +222,13 @@ DESC
 ###################################### 25 ##########################################
 
 #Найдите производителей принтеров, которые производят ПК с наименьшим объемом RAM
-#и с самым быстрым процессором среди всех ПК, имеющих наименьший объем RAM. Вывести: Maker
+#и с самым быстрым процессором среди всех ПК, имеющих наименьший объем RAM.
+#Вывести: Maker
 
 SELECT maker FROM Product
 	WHERE model IN (SELECT model FROM PC
-		WHERE speed = (SELECT MAX(speed) FROM PC WHERE ram = (SELECT MIN(ram) FROM PC))
+		WHERE speed = (SELECT MAX(speed) FROM PC
+            WHERE ram = (SELECT MIN(ram) FROM PC))
 		AND ram = (SELECT MIN(ram) FROM PC))
  	AND maker IN (SELECT p.maker FROM Product p WHERE p.type='Printer')
 GROUP BY maker
